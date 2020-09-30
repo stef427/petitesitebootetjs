ee467613b636ccc8c518d0fab81355b4b7fa3cf8
stef427 <stef_427@hotmail.com>
2020-09-30 17:00:45 +0200

    correction

--- script.js
+++ script.js
@@ -9,11 +9,29 @@
 //deuxième facon on appel le get element comme variable pour eviter de le refaire a chaque fois //
 let CardChange = document.getElementById("carte");
 
-CardChange.addEventListener("mouseenter", function(event) {
+CardChange.addEventListener("mouseenter", function(event) { //mouseenter tout l'element // mouover au passage chaque élément
     event.target.style.width = "20rem";
-    event.target.style.color = "orange";
-    event.target.style.width = "18rem";
+    event.target.style.color = "yellow";
+    event.target.style.boxShadow = " pink 5px 2px 2px"
     setTimeout(function() {
         event.target.style.color = "";
     }, 1500);
 }, false);
+
+let visible = false;
+
+function see() {
+
+
+
+    if (!visible) {
+        document.getElementById("cache").style.display = "block";
+        document.getElementById("see").innerHTML = "unsee me ";
+        visible = true;
+    } else if (visible) {
+        document.getElementById("cache").style.display = "none";
+        document.getElementById("see").innerHTML = "see me";
+        visible = false;
+    }
+
+}
